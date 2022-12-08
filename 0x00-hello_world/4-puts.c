5269136e8f81fbcb298d6b6aaa787de0ba641195
Davis Gitonga <davisgitonganyaga@gmail.com>
2022-12-08 12:21:19 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry piont
+ *
+ * Prints some text
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
