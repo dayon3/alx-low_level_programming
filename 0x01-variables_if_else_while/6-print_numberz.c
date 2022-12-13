97d8cc746d60c4c1d4b4e5d55b9ae42f94bbfe07
Davis Gitonga <davisgitonganyaga@gmail.com>
2022-12-13 12:06:23 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Print all single digit numbers of base 10
+ * starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchat(i + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
