d81e7f8fed271e1ea17eadeed4769577eb301d26
Davis Gitonga <davisgitonganyaga@gmail.com>
2022-12-14 12:16:30 -0800

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_row - prints a single row of the times table
+ * @row: row number
+ *
+ * Return: void
+ */
+
+void print_row(int row)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0' + row * i);
+	}
+	_putchar('\n');
+}
+
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		print_row(i);
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -39,3 +39,6 @@
 
 `8-24_hours.c` - A function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
 - Prototype: `void jack_bauer(void);`
+
+`9-times_table.c` - A function that prints the 9 times table, starting with 0.
+- Prototype: `void times_table(void);`
