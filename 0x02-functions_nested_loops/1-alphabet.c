c751fc4bed8f1855846bf127c70183663c3f0a8d
Davis Gitonga <davisgitonganyaga@gmail.com>
2022-12-14 04:29:03 -0800

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,7 +7,7 @@
  * Return: Always 0 (Success)
  */
 
-int print_alphabet(void)
+void print_alphabet(void)
 {
 	char c;
 
@@ -16,6 +16,4 @@ int print_alphabet(void)
 		_putchar(c);
 	}
 	_putchar('\n');
-
-	return (0);
 }
