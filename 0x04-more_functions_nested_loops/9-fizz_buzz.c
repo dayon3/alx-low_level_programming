09af3ec466e6a5a5db6a78fdb3a99c4ca3da04ac
Davis Gitonga <davisgitonganyaga@gmail.com>
2022-12-16 05:40:07 -0800

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,9 +5,11 @@
  * But for multiples of three print `Fizz` instead of the number
  * and for the multiples of five print `Buzz`. For numbers which are
  * multiples of both three and five print `FizzBuzz`.
+ *
+ * Return: 0
  */
 
-void main(void)
+int main(void)
 {
 	int i;
 
@@ -23,4 +25,6 @@ void main(void)
 			printf("%d ", i);
 	}
 	printf("\n");
+
+	return (0);
 }
