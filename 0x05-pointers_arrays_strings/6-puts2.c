cdd5bea6d3b402eaeaccbf25ba26a03bafd660f8
Davis Gitonga <davisgitonganyaga@gmail.com>
2022-12-20 05:32:26 -0800

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,12 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0, len = 0;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	while (str[i++])
+		len++;
+
+	for (i = 0; i < len; i += 2)
 	{
 		_putchar(str[i]);
 	}
