e249bc45464e51108ccf084745a3b7567d3ca0f9
Davis Gitonga <davisgitonganyaga@gmail.com>
2023-01-12 00:37:30 -0800

    Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,15 +12,16 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *arr;
 	unsigned int i;
 
 	if (size == 0)
-		return ('\0');
+		return (NULL);
 
-	char *arr = (char *)malloc(sizeof(char) * size);
+	arr = (char *)malloc(sizeof(char) * size);
 
-	if (arr == '\0')
-		return ('\0');
+	if (arr == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
