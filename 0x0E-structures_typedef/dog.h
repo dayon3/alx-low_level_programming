ba69058273a9927dad8d694f8674f01c6ee0ea6e
Davis Gitonga <davisgitonganyaga@gmail.com>
2023-01-17 11:21:50 -0800

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog 
+/**
+ * struct dog - poppy
+ * @name: name of the dog
+ * @age: dogs age
+ * @owner: name of the dog's owner
+ */
+
+struct dog
 {
 	char *name;
 	float age;
