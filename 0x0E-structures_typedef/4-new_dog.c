7a80e706f860193dfc37016d029228616a0672fc
Davis Gitonga <davisgitonganyaga@gmail.com>
2023-01-18 10:00:25 -0800

    A door is what a dog is perpetually on the wrong side of

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,40 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog.
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: NULL if the function fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog = malloc(sizeof(dog_t));
+	
+	if (new_dog == NULL)
+		return NULL;
+	
+	new_dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+	
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return NULL;
+	}
+	
+	strcpy(new_dog->name, name);
+	new_dog->age = age;
+	new_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return NULL;
+	}
+	
+	strcpy(new_dog->owner, owner);
+	return (new_dog);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct dog - poppy
@@ -22,5 +23,6 @@ typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
