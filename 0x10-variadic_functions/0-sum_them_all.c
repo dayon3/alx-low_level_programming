f2eb85a99119432c27b4965f95046ce049391926
Davis Gitonga <davisgitonganyaga@gmail.com>
2023-01-25 11:16:51 -0800

    One woman's constant is another woman's variable

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,4 @@
-#include <stdarg.h>
+#include "variadic_functions.h"
 
 /**
  * sum_them_all - Returns the sum of all its parameters
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Prints strings, followed by a new line
+ * @separator: string to be printed between the strings
+ * @n: number of strings passed to the function
+ * @...: Variable list of integers
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		char *str = va_arg(args, char*);
+
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", str);
+		}
+
+		if (separator != NULL && i < (n - 1))
+			printf("%s", separator);
+	}
+
+	va_end(args);
+	printf("\n");
+}
