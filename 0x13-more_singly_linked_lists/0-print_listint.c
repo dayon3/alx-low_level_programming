3947944b8e586169028d977ede4ab6d16ef4a94e
Davis Gitonga <davisgitonganyaga@gmail.com>
2023-02-03 09:56:40 -0800

    List length

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,7 +6,7 @@
  *
  * Return: the number of nodes
  */
-size_int print_listint(const listint_t *h)
+size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *current = h;
+
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+
+	return (count);
+}
